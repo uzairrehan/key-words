80e3192fd0018d75b6f5294788ce2d0389cfb596
Syed Uzair Shah <iamsyeduzairshah@gmail.com>
2024-05-13 18:21:46 +0500

    im practicing javascript

---
+++ app.js
@@ -0,0 +1,52 @@
+// All things that ive learned im practicing it in here
+
+console.log(123);
+
+document.write(123);
+
+let a = 1;
+
+const b = 2;
+
+var c = 3;
+
+prompt("hey there!") + prompt("hey there!");
+
+a++;
+
+++a;
+
+a--;
+
+--a;
+
+1 + 1;
+
+1 - 1;
+
+2 * 2;
+
+2 ** 2;
+
+2 / 2;
+
+2 % 2;
+
+2 > 1;
+
+1 < 2;
+
+11 >= 11;
+
+11 <= 11;
+
+11 == 11;
+
+11 === 11;
+
+11 !== 11;
+
+true 
+
+false
+
