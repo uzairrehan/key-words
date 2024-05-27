a20ba473dce504ee752cf5dc1eabcb1b008a86e2
Syed Uzair Shah <iamsyeduzairshah@gmail.com>
2024-05-27 10:29:23 +0500

    more

--- app.js
+++ app.js
@@ -197,3 +197,56 @@
 
 // primitive and reference
 
+// .appendChild(newElement); //append in last
+
+// .insertBefore(newElement, target); //insert before target elment
+
+// .setAttribute(attrName, attrValue) // set attribute to element node
+
+// .removeChild(target) // remove element node from parent node
+
+// DOM
+
+// finding children
+
+// junk artifact / text / spaces and linebreak 
+
+// document.getElementsByTagName()   hamesha array lata he
+
+// document.getElementsByClassName() hamesha array lata he
+
+// document.getElementById()         hamesha element lata he 
+
+// document.querySelectorAll()       hamesha array lata he
+
+// document.querySelector()          hamesha matching ka pehle element lata he
+
+// console.log(document.childNodes[1].childNodes[2].parentNode.childNodes[0].childNodes[5].innerHTML);
+
+// .childNodes
+
+// .parentNode
+
+// object 
+
+// element
+
+// array / nodelist / collection list
+
+// document.previousSibling.previousSibling
+
+// document.nextSibling.nextSibling
+
+// document.firstChild
+
+// document.lastChild
+
+// document.nodeType returns 1  and 3
+
+// document.nodeName returns tag name in capital
+
+// let h1 =  document.querySelectorAll("h1")
+
+// console.log(h1[0].id);
+
+// let h1 =  document.querySelector("h1");
