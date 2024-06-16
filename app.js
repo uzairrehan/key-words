49e3818bbb266985a0f521ed874a27f4ae9117cf
Syed Uzair Shah <iamsyeduzairshah@gmail.com>
2024-06-16 18:00:09 +0500

    updated

--- app.js
+++ app.js
@@ -46,15 +46,15 @@
 
 // 11 !== 11;
 
-// true 
+// true
 
 // false
 
 // confirm();
 
-// if 
+// if
 
-// else 
+// else
 
 // else if
 
@@ -72,15 +72,15 @@
 
 // array.indexOf()
 
-// array.pop() 
+// array.pop()
 
-// array.push() 
+// array.push()
 
-// array.shift()  
+// array.shift()
 
-// array.unshift() 
+// array.unshift()
 
-// array.splice() 
+// array.splice()
 
 // .concat
 
@@ -154,7 +154,7 @@
 
 // Number
 
-// \n	
+// \n
 
 // new Date()
 
@@ -209,13 +209,13 @@
 
 // finding children
 
-// junk artifact / text / spaces and linebreak 
+// junk artifact / text / spaces and linebreak
 
 // document.getElementsByTagName()   hamesha array lata he
 
 // document.getElementsByClassName() hamesha array lata he
 
-// document.getElementById()         hamesha element lata he 
+// document.getElementById()         hamesha element lata he
 
 // document.querySelectorAll()       hamesha array lata he
 
@@ -223,11 +223,11 @@
 
 // console.log(document.childNodes[1].childNodes[2].parentNode.childNodes[0].childNodes[5].innerHTML);
 
-// .childNodes
+// .childNodes[0]
 
 // .parentNode
 
-// object 
+// object
 
 // element
 
@@ -250,3 +250,190 @@
 // console.log(h1[0].id);
 
 // let h1 =  document.querySelector("h1");
+
+// turnery operator
+
+// condition ? ifblock : elseblock
+
+// functions key value pair
+
+// switch statement 
+
+// onmouseover=""
+
+// onmouseout=""
+
+// onblur="fillProvince()"
+
+//  onclick="divertUser(`https://www.google.com`)"
+
+// href="JavaScript:void(0)"
+// document.querySelectorAll();
+// document.querySelector();
+// document.getElementsByTagName();
+// document.getElementsByClassName();
+// document.getElementById();
+
+// document.childNodes;
+// document.parentNode;
+// document.nextSibling;
+// document.previousSibling;
+// document.firstChild;
+// document.lastChild;
+
+// document.nodeType // 1 or 3
+// document.nodeName // element name in upper case
+
+
+
+/*
+.appendChild(newElement); //append in last
+.insertBefore(newElement, target); //insert before target elment
+.setAttribute(attrName, attrValue) // set attribute to element node
+.removeChild(target) // remove element node from parent node
+*/
+
+// let target = divEl.firstChild.nextSibling;
+// divEl.removeChild(target);
+// divEl.appendChild(h1El);
+
+
+// let divEl = document.querySelectorAll('#container')[0];
+// let h1El = document.createElement("H1");
+// h1El.setAttribute("id", "main-heading");
+// let allKeys = Object.keys(human);
+// let allValues = Object.values(human);
+
+
+// let currentURL = location;
+// console.log(currentURL.pathname);
+// let allValues = Object.values(student1);
+// console.log(allValues.includes("Haider"));
+/*
+stack
+heap
+
+Mutable & Immutable
+
+primitive types are IMMUTABLE
+string, number, boolean, undefined, null, bigint, symbol
+
+
+non primitive types are MUTABLE
+object, array, function
+
+
+*/
+
+// copy values
+// let user2 = Object.assign({}, user1);
+// let favFruits = [...fruits];
+ 
+
+
+
+/*Promise*/
+
+// let treat = new Promise(function (resolve, reject) {
+//     let isNotesAvail = confirm('have you produced english notes');
+//     if (isNotesAvail) {
+//         let foodWish = prompt('what do you want to eat?');
+//         resolve(foodWish);
+//     }
+//     else {
+//         reject("ainda bat ni krna");
+//     }
+// });
+
+// treat
+//     .then(function (foodItem) {
+//         console.log(`You are my true friend, ${foodItem} is due on me.`);
+//     })
+//     .catch(function (e) {
+//         console.log('you are not my true friend.', e);
+//     });
+
+
+// const { userName: name, fathersName, rollNum, ...remainingProp } = student;
+
+
+// let fruits = ['Apple', 'Banana', "Watermelon", 'Oranges', 'Mango'];
+
+// const [fruit1, , , fruit2] = fruits;
+
+// const [fruit1, , fruit2, , fruit3] = fruits;
+
+// const [, , , , kingFruit] = fruits;
+
+/*
+arrow function
+
+()=>{};
+
+()=> "happy birthday";
+
+const sum = (num1, num2) => `sum of ${num1} and ${num2} is ${num1 + num2}`;
+
+userName => `Welcome ${userName}!`;
+
+
+*/
+
+// .forEach()
+
+// .map()
+
+// .filter()
+
+// .includes()
+
+// .reduce();
+
+// .some()
+
+// .every()
+
+// "every" and "some" method returns bolean  
+// "every" check  all 
+// "some" check only one
+// reduce method returns  
+// function in object is called method
+// arrow function dont have "this" keyword
+// map is just like foreach but it always retuns a value and makes new array from existing array .
+// this 
+// destructuring of object
+// rest operator
+// spread operator
+// destructuring of array
+
+// arrow functions and dont have this keyword
+// this is used for callback
+// also you can name it
+
+// arrow agar aik line ka hai or kuch return bi ho raha he to is tarh likhenge .this is called implicit return
+// const greet = () => prompt("hello");
+
+// agar aik parameter ho to is tarha function banate heen or return bhi karana ho to
+// const makeGreeting = userName => `Welcome ${userName}`
+// forEach loop this is only for array
+// for In loop this is for object
+// do while loop jab kam az kam aik bar chalana ho
+
+//  imediately invoked function rxpression (IIFE)
+// global scope ke pollutoion ko hatane keliyen use hota he aur forna baad call karne ke liyen
+
+// khatam hone ke baad semicolon ";" zaroor lagana he end karne ke liyen
+
+
+
+// named IIFE
+// (function code() {
+//     console.log("hello");
+// })();
+
+
+
+// with arrow function 
+// ((name) => {
+//     console.log("hello " + name);
+// })("uzair");
